5330f62ed15dedfc92d07a940078c7c3f0bbf1f7
Frah01 <vinario.francesco@gmail.com>
2023-01-10 14:36:53 +0100

    Esercizio completo con bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'AppHero',
+    data(){
+        return{
+            hello2: 'Benvenuto'
+        }
+    }
+}
+</script>
+<template lang="">
+    <div class="font">
+        <img src="/jumbo.png">
+    </div>
+</template>
+<style scoped>
+    .font{
+        font-weight: bold;
+    }
+    img {
+        width: 100%;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+     name: "AppTitle",
+     data(){
+        return{
+            hello: 'Ciao'
+        }
+     },       
+}
+</script>
+
+<template lang="">
+    <div class="font">
+        {{hello}}
+        <img src="../assets/vue.svg" alt="">
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
